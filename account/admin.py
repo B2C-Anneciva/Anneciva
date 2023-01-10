64e6afcfdcc3d764ce2954abd5638b60b1343fa4
B2C-Anneciva <B2C-Anneciva@gmail.com>
2023-01-11 03:47:55 +0500

    ..

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from account.models import CustomerUser
 
-admin.site.register(CustomerUser)
+@admin.register(CustomerUser)
+class CustomerUserAdmin(UserAdmin):
+    # readonly_fields = ('username', 'email', 'password', 'country', 'company_name', 'user_type', 'phone_number', 'corporate_number')
+    list_display = ('username', 'email')

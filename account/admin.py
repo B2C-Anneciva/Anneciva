703dc9f6d70a1a282d2a4ccfb43de35cd472ff66
B2C-Anneciva <B2C-Anneciva@gmail.com>
2023-01-13 06:42:39 +0500

    Admin Panel

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from account.models import CustomerUser
 
-admin.site.register(CustomerUser)
+# admin.site.register(CustomerUser)
+@admin.register(CustomerUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email' , 'user_type')
+    search_fields = ('user_type', 'username', 'email')

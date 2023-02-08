9a5f9053220ca976827c72f9175d6c8e42c7dd8c
B2C-Anneciva <B2C-Anneciva@gmail.com>
2023-02-08 21:22:44 +0500

    Searching and filters

--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions, status, viewsets
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from product.models import Product
 from product.serializers import ProductSerializer
@@ -12,6 +12,9 @@ class ProductView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['category_id', 'name', 'country']
+    search_fields = ['name']
 
 class ProductDetailView(generics.ListAPIView):
 
